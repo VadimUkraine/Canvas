1ba8a4fc3ee7df552ca26f1494b295c455169458
Vadim <dim779564@gmai.com>
2020-10-13 18:04:35 +0300

    add drag and drop mechanism

--- js/script.js
+++ js/script.js
@@ -1,79 +1,225 @@
-
-document.addEventListener('dragstart', function(event) {
-  let targetWidth = event.target.getAttribute('data-width');
-  // event.target.classList.add('drag-item');
-  // let rectangle = document.createElement('p');
-  // rectangle.className = "drag-item";
-  // document.body.append(rectangle);
-  // rectangle.style.cssText = "color: blue; border: 1px solid black, width: "+targetWidth+", height: 30px, "; 
+// TODO
+// 1. code refactoring -DRY/YAGNI/KISS
+// 2. create constants for colors
+// 3. code spliting on functions
+// 4. 
 
 
-  console.log('DRAG ---', event.pageX, event.pageY)
 
+const canvas = document.getElementById('test');
+
+const rectangles=[
+  {
+    x: 10,
+    y: 10,
+    color: "grey",
+    width: 100,
+    height: 30,
+    isDraggable: false,
+    idx: 0,
+  },
+  {
+    x: 10,
+    y: 50,
+    color: "grey",
+    width: 50,
+    height: 30,
+    isDraggable: false,
+    idx: 1,
+  },
+  {
+    x: 10,
+    y: 90,
+    color: "grey",
+    width: 25,
+    height: 30,
+    isDraggable: false,
+    idx: 2,
+  },
+];
+
+let dragItemDefault={};
+
+window.addEventListener("load", function() {
+  setCanvasDimensions();
+  draw();
+})
+
+function setCanvasDimensions(){
+  let canvasWrapper = document.querySelector('.canvas-wrap');
+  let canvasWrapperRect = canvasWrapper.getBoundingClientRect();  
+
+  canvas.setAttribute("width", Math.round(canvasWrapperRect.width - 10));
+  canvas.setAttribute("height", Math.round(canvasWrapperRect.height - 10));
+}
+
+
+canvas.addEventListener('mousedown', function(event) {
+
+  let canvasRect = canvas.getBoundingClientRect(); 
+  let mouseX = Math.round(event.clientX - canvasRect.left);
+  let mouseY = Math.round(event.clientY - canvasRect.top);
+
+  rectangles.forEach((item, index)=>{
+    let xItemMaxPos = item.x+item.width
+    let yItemMaxPos = item.y+item.height
+    if(mouseX > item.x && mouseX < xItemMaxPos && mouseY > item.y && mouseY < yItemMaxPos ){
+      rectangles[index].isDraggable = true;
+    } else{
+      rectangles[index].isDraggable = false;
+    }
+  })
+
+  let dragItem = rectangles.find(item => item.isDraggable)
+  
+
+  if(dragItem){
+    dragItemDefault = dragItem;
+  
+    let ctx = canvas.getContext('2d');
+
+    ctx.clearRect(dragItem.x, dragItem.y, dragItem.width, dragItem.height);
+    ctx.fillStyle = "orange";
+    ctx.fillRect(dragItem.x, dragItem.y, dragItem.width, dragItem.height);
+  }
+  
 })
 
 
+canvas.addEventListener('mousemove', function(event) {
+  let dragItem = rectangles.find(item => item.isDraggable)
+  
 
 
-// TODO - это тестовая версия с Канвасом, удалю закомиченный код, когда разберусь как отрисовать 
-// прямоугольники по задаче
-window.onload = draw();
+  if(dragItem){    
+   
+    let canvasRect = canvas.getBoundingClientRect(); 
+    let ctx = canvas.getContext('2d');
+    ctx.clearRect(dragItem.x, dragItem.y, dragItem.width, dragItem.height);
 
-function draw(){
-  var canvas = document.getElementById('test');
+    let xPos = Math.round(event.clientX - canvasRect.left) - dragItem.width/2;
+    let yPos = Math.round(event.clientY - canvasRect.top) - dragItem.height/2;
 
-  if (canvas.getContext){
-    var ctx = canvas.getContext('2d');
-    console.log('CANVAS ----', getComputedStyle(canvas).width, getComputedStyle(canvas).height)
+    let newRectangle = {
+        x: xPos,
+        y: yPos,
+        color: "orange",
+        width: dragItem.width,
+        height: dragItem.height,
+        isDraggable: true,
+        idx: dragItem.idx,
+    }
 
-    // ctx.fillStyle = "red";
-    // ctx.fillRect(10, 10, 21, 11);
-    // ctx.clearRect(11,11,19,9);
-    // ctx.fillStyle = "red";
-    // ctx.fillRect(11, 11, 19, 9);
+    rectangles[dragItem.idx] = newRectangle;
 
-    // ctx.beginPath();
-    // ctx.moveTo(75,50);
-    // ctx.lineTo(100,75);
-    // ctx.lineTo(100,25);
-    // ctx.fillStyle = "red";
-    // ctx.fill()
+
+
+    // checking the crossings of rectanges
+
+    rectangles.forEach(dragItem=>{
+      const noDraggableItems = rectangles.filter(item=> !item.isDraggable)
+ 
+      if(dragItem.isDraggable){
+        noDraggableItems.forEach(noDragItem => {
+          if (dragItem.x < noDragItem.x + noDragItem.width &&
+            dragItem.x + dragItem.width > noDragItem.x &&
+            dragItem.y < noDragItem.y + noDragItem.height &&
+            dragItem.height + dragItem.y > noDragItem.y) {
+              rectangles[dragItem.idx].color="red"
+              rectangles[noDragItem.idx].color="red"
+          } else {
+            rectangles[noDragItem.idx].color="grey"
+          }
+        })
+      }
+   
+    })
+
+    // snapping
+
+    rectangles.forEach(dragItem=>{
+      const noDraggableItems = rectangles.filter(item=> !item.isDraggable)
     
 
-    // let rectangle = new Path2D();
+      if(dragItem.isDraggable){
+        noDraggableItems.forEach(noDragItem => {
+          if (dragItem.x <= noDragItem.x + noDragItem.width + 10 &&
+              dragItem.x + dragItem.width >= noDragItem.x - 10 &&
+              dragItem.y <= noDragItem.y + noDragItem.height + 10 &&
+              dragItem.height + dragItem.y >= noDragItem.y - 10) {
+              console.log('we can do SNAPPING ---', dragItem.x, dragItem.y, noDragItem.x, noDragItem.y)
+              
+              if(dragItem.x > noDragItem.x + dragItem.width){
+                if(dragItem.y + dragItem.height < noDragItem.y - 10){
+                  rectangles[dragItem.idx].x = noDragItem.x;
+                  rectangles[dragItem.idx].y = noDragItem.y - noDragItem.height
+                }
+                if(dragItem.y + dragItem.height > noDragItem.y + noDragItem.height + 10){
+                  rectangles[dragItem.idx].x = noDragItem.x + noDragItem.width;
+                  rectangles[dragItem.idx].y = noDragItem.y + noDragItem.height
+                }
+                if(dragItem.y + dragItem.height < noDragItem.y + noDragItem.height + 10 && dragItem.y + dragItem.height > noDragItem.y -10 ){
+                  rectangles[dragItem.idx].x = noDragItem.x + noDragItem.width;
+                  rectangles[dragItem.idx].y = noDragItem.y;
+                }
+              }
 
-    // rectangle.fillStyle = "red";
-    // rectangle.rect(10, 10, 21, 11);
-    // ctx.stroke(rectangle)
-
-
-    // ctx.imageSmoothingEnabled = false;
-    // ctx.fillStyle = "red";
-    // // ctx.lineWidth = 5;
-    // // // ctx.strokeStyle = "blue";
-    // ctx.fillRect(10, 10, 21, 11);
+          } 
    
-    // ctx.strokeStyle = "blue";
-    // ctx.beginPath();
-    // ctx.moveTo(10);
-    // ctx.lineTo(11);
-    // ctx.stroke();
-
-    // for (var i = 0; i < 10; i++){
-    //   ctx.lineWidth = 5;
-    //   ctx.beginPath();
-    //   ctx.moveTo(5+i*14,5);
-    //   ctx.lineTo(5+i*14,140);
-    //   ctx.stroke();
-    // }
-
-    // var img = new Image();
-    // ctx.drawImage(img, 20, 10)
-    // img.style.color = 'blue';
+        })
+      }
+   
+    })
 
 
 
 
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    rectangles.forEach(item =>{   
+      drawRectangle(ctx, item)
+    })  
+  }
+})
+
+canvas.addEventListener('mouseup', function(event) {
+  let dragItem = rectangles.find(item => item.isDraggable);
+
+  const isCrossing = rectangles.some(item=>item.color==="red")
+
+
+  if(isCrossing){
+     rectangles[dragItemDefault.idx] = dragItemDefault
+  }
+
+
+  if(dragItem){
+    let ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    rectangles.forEach(item =>{ 
+      item.isDraggable = false;  
+      item.color = "grey"
+      drawRectangle(ctx, item)
+    })
+  }
+})
+
+
+function drawRectangle(canvas, item){  
+  canvas.fillStyle = item.color;
+  canvas.fillRect(item.x, item.y, item.width, item.height);
+}
+
+function draw(){
+
+  if (canvas.getContext){
+    let ctx = canvas.getContext('2d');
+
+    rectangles.forEach(item =>{   
+      drawRectangle(ctx, item)
+    })   
+  
 
   }
 }
